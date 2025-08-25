db7c48f65ae946baeeff7070d84b966c06db5b99
dmaurex <dmaurex@gmail.com>
2025-08-25 23:56:38 +0200

    Implement the CCIP pool contract

--- src/Vault.sol
+++ src/Vault.sol
@@ -22,7 +22,8 @@ contract Vault {
      * @notice Deposit ETH to the vault and mint rebase tokens in return
      */
     function deposit() external payable {
-        i_rebaseToken.mint(msg.sender, msg.value);
+        uint256 interestRate = i_rebaseToken.getInterestRate();
+        i_rebaseToken.mint(msg.sender, msg.value, interestRate);
         emit Deposit(msg.sender, msg.value);
     }
 
--- src/interfaces/IRebaseToken.sol
+++ src/interfaces/IRebaseToken.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.30;
 
 interface IRebaseToken {
     function balanceOf(address _user) external view returns (uint256);
-    function mint(address _to, uint256 _amount) external;
+    function mint(address _to, uint256 _amount, uint256 _interestRate) external;
     function burn(address _from, uint256 _amount) external;
+    function getInterestRate() external view returns (uint256);
 }
